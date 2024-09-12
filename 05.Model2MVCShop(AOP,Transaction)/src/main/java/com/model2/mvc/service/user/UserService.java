6ef4159020acb0ec6cc80d769649911c7888ad02
afree <afree@DESKTOP-21RGT2C>
2024-09-12 16:42:19 +0900

    test

--- 05.Model2MVCShop(AOP,Transaction)/src/main/java/com/model2/mvc/service/user/UserService.java
+++ 05.Model2MVCShop(AOP,Transaction)/src/main/java/com/model2/mvc/service/user/UserService.java
@@ -8,7 +8,7 @@ import com.model2.mvc.service.domain.User;
 
 //==> 회원관리에서 서비스할 내용 추상화/캡슐화한 Service  Interface Definition  
 public interface UserService {
-	
+	  
 	// 회원가입
 	public void addUser(User user) throws Exception;
 	
